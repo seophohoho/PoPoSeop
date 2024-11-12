44e1226748be5c822e3163210743db19a274afa0
seophohoho <seop0937@gmail.com>
2024-11-12 19:31:20 +0900

    refactor(config): Migrate .eslintrc to ESM

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,37 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-  parserOptions: {
-    project: './tsconfig.json', // TypeScript 프로젝트 설정 파일 경로
-    sourceType: 'module',
-    ecmaVersion: 2020,
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: './tsconfig.json',
-      },
+export const parser = '@typescript-eslint/parser';
+export const extends = [
+  'airbnb-typescript/base',
+  'plugin:@typescript-eslint/recommended',
+  'prettier',
+  'plugin:import/errors',
+  'plugin:import/warnings',
+  'plugin:import/typescript',
+];
+export const parserOptions = {
+  project: './tsconfig.json', // TypeScript 프로젝트 설정 파일 경로
+  sourceType: 'module',
+  ecmaVersion: 2020,
+};
+export const settings = {
+  'import/resolver': {
+    typescript: {
+      alwaysTryTypes: true,
+      project: './tsconfig.json',
     },
   },
-  rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        ts: 'never',
-      },
-    ],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    '@typescript-eslint/no-unused-vars': 'warn', // 불필요한 변수 사용 경고
-    'prefer-const': 'error', // const 사용 권장
-  },
+};
+export const rules = {
+  'import/extensions': [
+    'error',
+    'ignorePackages',
+    {
+      js: 'never',
+      ts: 'never',
+    },
+  ],
+  'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  '@typescript-eslint/no-unused-vars': 'warn', // 불필요한 변수 사용 경고
+  'prefer-const': 'error', // const 사용 권장
+  'no-useless-constructor': 'off',
+  '@typescript-eslint/no-useless-constructor': 'error',
 };
